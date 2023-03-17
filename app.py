6eefbc7e10147237c8f5540c0c113ed0add2cc55
ggonzalezorellana <gago196@gmail.com>
2023-03-17 19:10:07 -0300

    [IMP] Beging 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from utils.building import divide_teams
 from utils.db_get.get_info import get_available_players
 
@@ -9,8 +9,16 @@ def build_teams():
     # Code
 
     players = get_available_players()
-
     
+    player_list = []
+    for player in players:
+        player_dict = {
+            'name': player[0]
+        }
+        player_list.append(player_dict)
 
-    # Return teams
-    return 'Done'
+    # return the list of players as JSON
+    return jsonify(player_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
